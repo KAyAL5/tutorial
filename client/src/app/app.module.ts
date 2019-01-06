dd121153cc6488e60e69808d6388b35fd0a0e7ca
KAyAL5 <subscribedservice@gmail.com>
2019-01-06 23:21:07 +0530

    feat(CLT-Routes): Angular Route implement.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
 import { AppRoutingModule } from './modules/app.routing.module';
 
-import { JwtInterceptor, ErrorInterceptor } from './guards';
-
-import { AuthService, NotificationService } from './services/shared';
-
 import { AppComponent } from './app.component';
 import { LoginComponent, RegistrationComponent, TopnavbarComponent } from './components/general';
 
@@ -29,12 +25,7 @@ import { LoginComponent, RegistrationComponent, TopnavbarComponent } from './com
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule],
-  providers: [
-    AuthService,
-    NotificationService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
