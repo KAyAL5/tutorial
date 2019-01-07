372359522d648e3dfd3a3d15b4301222bd55b1e1
KAyAL5 <subscribedservice@gmail.com>
2019-01-07 21:36:05 +0530

    feat(CLT-Routes): Angular Route implement.

--- client/src/app/modules/app.routing.module.ts
+++ client/src/app/modules/app.routing.module.ts
@@ -1,16 +1,15 @@
 import {NgModule}  from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from '../guards';
-
-import {LoginComponent, RegistrationComponent} from '../components/general';
+import {LoginComponent} from '../components/general';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
   { path: 'login', component: LoginComponent },
-  { path : 'signin', component : RegistrationComponent},
-  { path: 'teacher', canActivate: [AuthGuard], loadChildren: './teacher.module#TeacherModule' },
-  { path: 'student', canActivate: [AuthGuard], loadChildren: './student.module#StudentModule' },
+  { path : '', component : LoginComponent},
+  { path: 'teacher', loadChildren: './teacher.module#TeacherModule' },
+  { path: 'student', loadChildren: './student.module#StudentModule' },
+  { path: '404', component: LoginComponent},
+  { path: '**', redirectTo: '/login'}
 ];
 
 @NgModule({
